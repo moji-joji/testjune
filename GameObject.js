8b5701b6f4c2c652cce9995d7e4fcb93e0280267
moji-joji <makbar.bscs20seecsm>
2022-06-20 12:54:26 +0500

    2

--- GameObject.js
+++ GameObject.js
@@ -7,7 +7,9 @@ class GameObject {
     this.direction = config.direction || "down";
     this.sprite = new Sprite({
       gameObject: this,
-      src: config.src || "images/characters/people/hero.png",
+      src:
+        config.src ||
+        "https://github.com/moji-joji/testjune/blob/master/images/characters/people/hero.png",
     });
 
     this.behaviorLoop = config.behaviorLoop || [];
