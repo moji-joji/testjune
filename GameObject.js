200b94306f0b72fe979ebc31bec07642ab33e3fe
moji-joji <makbar.bscs20seecsm>
2022-06-20 12:52:09 +0500

    1

--- GameObject.js
+++ GameObject.js
@@ -7,30 +7,27 @@ class GameObject {
     this.direction = config.direction || "down";
     this.sprite = new Sprite({
       gameObject: this,
-      src: config.src || "/images/characters/people/hero.png",
+      src: config.src || "images/characters/people/hero.png",
     });
 
     this.behaviorLoop = config.behaviorLoop || [];
     this.behaviorLoopIndex = 0;
-
   }
 
   mount(map) {
-    console.log("mounting!")
+    console.log("mounting!");
     this.isMounted = true;
     map.addWall(this.x, this.y);
 
     //If we have a behavior, kick off after a short delay
     setTimeout(() => {
       this.doBehaviorEvent(map);
-    }, 10)
+    }, 10);
   }
 
-  update() {
-  }
+  update() {}
 
-  async doBehaviorEvent(map) { 
-
+  async doBehaviorEvent(map) {
     //Don't do anything if there is a more important cutscene or I don't have config to do anything
     //anyway.
     if (map.isCutscenePlaying || this.behaviorLoop.length === 0) {
@@ -43,19 +40,15 @@ class GameObject {
 
     //Create an event instance out of our next event config
     const eventHandler = new OverworldEvent({ map, event: eventConfig });
-    await eventHandler.init(); 
+    await eventHandler.init();
 
     //Setting the next event to fire
     this.behaviorLoopIndex += 1;
     if (this.behaviorLoopIndex === this.behaviorLoop.length) {
       this.behaviorLoopIndex = 0;
-    } 
+    }
 
     //Do it again!
     this.doBehaviorEvent(map);
-    
-
   }
-
-
 }
